1bb33489da4de645265368a4584dc53d400744d1
Yurubhillow <hillowyurub@gmail.com>
2026-01-02 08:39:53 +0300

    Day 2: learned variables

---
+++ day01_intro/day02_basics/day2_basics.py
@@ -0,0 +1,6 @@
+# variable naming
+student_count = 1000
+print(student_count)
+rating = 4.99
+is_published = True
+course_name = "python programing"
--- day01_intro/day1_intro.py
+++ day01_intro/day1_intro.py
@@ -1,2 +1,9 @@
 print("Bismillah")
 print("Day 1: My Python journey starts today")
+
+x = 1
+y = 2
+unit_price = 3
+v = 5
+s = x+y+v+unit_price
+print(s)
