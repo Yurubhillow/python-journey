736bdab8ece9b2396a30d6c841af34b305cc9d13
Yurubhillow <hillowyurub@gmail.com>
2026-01-05 09:17:10 +0300

    Day 3: input, output and basic math

---
+++ day01_intro/day02_basics/day3_input_output/day3_input_output.py
@@ -0,0 +1,20 @@
+# user info
+
+name = input("What is your name? ")
+age = input("How old are you? ")
+
+print("Hello", name)
+print("You are", age, "years old")
+
+# simple math
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+sum = num1 + num2
+print("The sum is:", sum)
+
+# (Real Life – Mommy Friendly 💙)
+
+child_name = input("Enter your child's name: ")
+print(child_name, "is loved and protected ❤️")
